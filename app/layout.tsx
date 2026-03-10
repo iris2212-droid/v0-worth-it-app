1ef7965dbe750171f90ad89a3c78f459ea01f863
iris2212-droid <iris2212@gmail.com>
2026-03-10 18:47:45 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   other: {
     "impact-site-verification": "11c4d98e-c4d4-48ca-96ef-5cb368827ab4"
   }
-},
+}
 
 export default function RootLayout({
   children,
