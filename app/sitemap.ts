d6d89842850684ab7899d9f73869ae61e53c07a3
iris2212-droid <iris2212@gmail.com>
2026-03-19 12:09:26 +0100

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://worthitindex.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://worthitindex.com/hysa-calculator',
+      lastModified: new Date(),
+    },
+  ]
+}
